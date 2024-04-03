7d4af30419d57cadfe632f593277185e7af576e7
Ali-Taqi <149289506+Ali-Taqi@users.noreply.github.com>
2024-04-03 11:34:21 +0000

    VIDOE 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
 import './App.css';
-import HomePage from "./pages/HomePage";
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
+import HomePage from "./pages/home/HomePage";
+
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div className="App font-opensans">
+     <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/blog/:id" element={<ArticleDetailPage />} />
+      </Routes>
     </div>
   );
 }
