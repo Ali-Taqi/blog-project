27e12d1280b877b27a03229e75a0a01063f7e04f
Ali-Taqi <149289506+Ali-Taqi@users.noreply.github.com>
2024-04-08 12:08:59 +0000

    vidoe 19,20

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import store from "./store";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </BrowserRouter>
 );
--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ArticleCard from "../../../components/ArticleCard";
 import { FaArrowRight } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { changeCount } from "../../../store/actions/countActions";
+
 const Articles = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+
+  const countChangeHandler = (type) => {
+    dispatch(changeCount(type));
+  };
+
   return (
     <section className="flex flex-col container mx-auto px-5 py-10">
-      <div className=" flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
+      <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
         <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
       </div>
@@ -12,6 +22,11 @@ const Articles = () => {
         <span>More articles</span>
         <FaArrowRight className="w-3 h-3" />
       </button>
+      <div className="mt-2 flex items-center gap-x-5">
+        <button onClick={() => countChangeHandler("DECREASE")}>decrease</button>
+        {count.number}
+        <button onClick={() => countChangeHandler("INCREASE")}>increase</button>
+      </div>
     </section>
   );
 };
